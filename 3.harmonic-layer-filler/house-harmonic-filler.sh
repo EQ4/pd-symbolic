a9a3b327851f2f33454b09a3295ba2accb166b7a
angelfaraldo@gmail.com <angelfaraldo@gmail.com>
2015-09-30 11:50:07 +0200

    new interface

--- 3.harmonic-layer-filler/house-harmonic-filler.sh
+++ 3.harmonic-layer-filler/house-harmonic-filler.sh
@@ -4,9 +4,10 @@
 STRING="Harmonic Layer Filler"
 echo $STRING
 
-pd-extended --nogui open "./harmonic-layer-filler.pd" &
-sleep 0.5
-wish hlf.tk
+pd-extended -noaudio open "./hhf-pd.pd" &
+python hhf-gui.py
+killall python
 killall pd-extended
 killall pd
 exit
+
